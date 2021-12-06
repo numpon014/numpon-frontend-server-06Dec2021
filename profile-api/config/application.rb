cfaded6ee20b12f04f30baa258142a604f8b0f4c
numpon014 <numpon014@gmail.com>
2021-12-06 22:24:32 +0700

    added health check and relative path

--- profile-api/config/application.rb
+++ profile-api/config/application.rb
@@ -24,6 +24,7 @@ module ProfileApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.relative_url_root = "/api"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- profile-api/config/routes.rb
+++ profile-api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  health_check_routes
+
   resources :users
   resources :experiences
   post 'login', to: 'authentication#login'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
